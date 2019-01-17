d171ada82364ee9a7fa9262553ed88454370e376
Hardik Nakarani <hdnakarani@gmail.com>
2019-01-17 15:37:20 +0530

    css ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'app-server',
-    templateUrl:'./server.component.html'
+    templateUrl:'./server.component.html',
+    styles:[`
+    .online{
+        color:white
+    }
+    
+    `]
 })
 export class ServerComponent{
 serverId:number=10;
@@ -17,6 +23,6 @@ getStatus(){
 }
 
 getColor(){
-    return this.status==='online'?'green':'red';
+    return this.status === 'online'?'green':'red';
 }
 }
